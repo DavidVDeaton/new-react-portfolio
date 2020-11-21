626f4921e9edd64d1998b5c01cd952be4087da4c
David Deaton <dideater@gmail.com>
2020-11-20 18:43:28 -0800

    trying to deploy

---
+++ client/build/precache-manifest.6a7807ff549204173a7e8ce8572c5ceb.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c62d21bd9fcad1abfa44d9d957dea2d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c45fc5fb222be604209",
+    "url": "/static/css/main.52b26f76.chunk.css"
+  },
+  {
+    "revision": "5a2c00e87a15bde9c3fb",
+    "url": "/static/js/2.4c793cde.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.4c793cde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c45fc5fb222be604209",
+    "url": "/static/js/main.dff12831.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  },
+  {
+    "revision": "a103126cbc435e5bd465e44db3595e10",
+    "url": "/static/media/Homework10Screenshot.a103126c.PNG"
+  },
+  {
+    "revision": "5ab1fecfcc5564f85a8876e1a35f0af0",
+    "url": "/static/media/WorkDayScheduler.5ab1fecf.png"
+  },
+  {
+    "revision": "98ebee177433d222e659bb11c53a69c2",
+    "url": "/static/media/endlessfantasysports.98ebee17.PNG"
+  },
+  {
+    "revision": "8e692de5252fbc326e12f67f9b2cbab1",
+    "url": "/static/media/questions.8e692de5.PNG"
+  },
+  {
+    "revision": "7c723778e611fdd19969aa2b7c2321d5",
+    "url": "/static/media/screenshot_large.7c723778.png"
+  },
+  {
+    "revision": "299b3baf3ea42a714bdd860d35b9c094",
+    "url": "/static/media/weatherdashboard.299b3baf.PNG"
+  }
+]);
